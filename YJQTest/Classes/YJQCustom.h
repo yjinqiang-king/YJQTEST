ee8c302a95f928811619633d3b0dd419794d8a67
yjq <752773744@qq.com>
2019-05-08 14:29:29 +0800

    addTestMethod

---
+++ YJQTest/Classes/YJQCustom.h
@@ -0,0 +1,18 @@
+//
+//  YJQCustom.h
+//  Pods-YJQTest_Example
+//
+//  Created by yjq on 2019/5/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YJQCustom : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YJQTest/Classes/YJQCustom.m
@@ -0,0 +1,16 @@
+//
+//  YJQCustom.m
+//  Pods-YJQTest_Example
+//
+//  Created by yjq on 2019/5/8.
+//
+
+#import "YJQCustom.h"
+
+@implementation YJQCustom
+
+-(void)test {
+    NSLog(@"It is OK");
+}
+
+@end
